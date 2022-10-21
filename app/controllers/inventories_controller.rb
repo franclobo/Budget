c905533805deeb8a9c440c31d90d7abd93d2d72b
franclobo <fjbl2788@gmail.com>
2022-10-21 00:07:04 -0500

    Edit views

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,10 +1,36 @@
 class InventoriesController < ApplicationController
   def index
+    @inventories = Inventory.includes([:classes]).where(author: current_user.id)
   end
 
   def show
+    @inventory = Inventory.find(params[:id])
+    @item = @inventory.item
   end
 
   def create
+    @inventory = Inventory.new(inventory_params)
+    @inventory.author = current_user.id
+    if @inventory.save
+      redirect_to inventory_path(@inventory)
+    else
+      render :new
+    end
+  end
+
+  def most_recent
+    @inventories = Inventory.includes([:classes]).where(author: current_user.id).order(created_at: :desc)
+    render :index
+  end
+
+  def most_ancient
+    @inventories = Inventory.includes([:classes]).where(author: current_user.id).order(created_at: :asc)
+    render :index
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name, :icon)
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,21 @@
 class ItemsController < ApplicationController
   def new
+    @inventory = Inventory.find(params[:inventory_id])
+    @item = Item.new
+    @inventories = Inventory.where(user:id: current_user.id)
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.author = current_user.id
+    @inventory = Inventory.find(params[:inventory_id])
+    if params[:inventories_ids]
+      params[:inventories_ids].each do |inventory_id|
+        Inventory.find(inventory_id).items << @item
+      end
+    end
+    redirect_to inventory_path
+  else
+    redirect_to new_inventory_item_path(@inventory)
   end
 end
